0a802ba1fb3a9c5681363190cacfaba4be9e4a5a
oka <oka@stu.kobe-u.ac.jp>
2021-05-03 02:10:38 +0900

    add env sensor

--- arduino/co2-monitor/README.md
+++ arduino/co2-monitor/README.md
@@ -1,9 +1,21 @@
 # M5Stack CO2 monitor (Arduino / Platform IO)
 
-## ソースコード配布元
+## ソースコード参照元
 
-- [M5Stack + MH-Z19B でCO2モニタ作成(ソースコードあり) | Violet Mist](https://swada.net/monitoring-co2-by-mhz19b/)
+- [M5Stack + MH-Z19B でCO2モニタ作成(ソースコードあり) | Violet Mist](https://swada.net/monitoring-co2-by-mhz19b/)  
+  MH-Z19 の読み取りとグラフ作成。
+- [DHT12 by Rob Tillaart · Libraries · PlatformIO](https://platformio.org/lib/show/5554/DHT12)  
+  DHT12 の読み方の例をそのまま利用した。
+- [M5Stack Core2にSWITCHSCIENCE BME280モジュールをGrove端子に接続して温度/湿度/気圧の測定をしてみる。](https://ak1211.com/7702/)
+  - 最初参考にしたけど M5Stack Core2 で新しかったので動かなかった。LovyanGFX の使い方は参考になった。
+- [M5-ProductExampleCodes/ENV.ino at master · m5stack/M5-ProductExampleCodes](https://github.com/m5stack/M5-ProductExampleCodes/blob/master/Core/m5go/m5go_lite/Arduino/ENV/ENV.ino)
+
 ## 使用ライブラリ
 
 - [WifWaf/MH-Z19: For Arduino Boards (&ESP32). Additional Examples/Commands., Hardware/Software Serial](https://github.com/WifWaf/MH-Z19)
   - Platform IO では → [MH-Z19 by Jonathan Dempsey · Libraries · PlatformIO](https://platformio.org/lib/show/6091/MH-Z19)
+- [xreef/DHT12_sensor_library: DHT12 complete library](https://github.com/xreef/DHT12_sensor_library)
+  - Platform IO では → [DHT12 sensor library by Renzo Mischianti · Libraries · PlatformIO](https://platformio.org/lib/show/11158/DHT12%20sensor%20library)
+- [adafruit/Adafruit_BMP280_Library: Arduino Library for BMP280 sensors](https://github.com/adafruit/Adafruit_BMP280_Library)  
+  BMP280 と BME280 でややこしいけど、使っている M5Stack の ENV ver.1 は BMP280 っぽい。
+  - Platform IO では → - [Adafruit BMP280 Library by Adafruit · Libraries · PlatformIO](https://platformio.org/lib/show/528/Adafruit%20BMP280%20Library)
--- arduino/co2-monitor/src/main.cpp
+++ arduino/co2-monitor/src/main.cpp
@@ -1,5 +1,30 @@
 #include <M5Stack.h>
-#include "MHZ19.h"                                        
+#include <Adafruit_Sensor.h>
+#include <Adafruit_BMP280.h>
+#include <MHZ19.h>
+#include <Wire.h> 
+#include <DHT12.h>
+
+//#define LGFX_M5STACK
+#include <LovyanGFX.hpp>
+static LGFX lcd;
+
+// Pressure sensor
+static const uint8_t BMP280_I2C_ADDRESS = 0x76;
+//static const uint8_t BMP280_I2C_ADDRESS = 0x77;
+static Adafruit_BMP280 bmp280;
+
+// temperature and humidity sensor
+DHT12 dht12(&Wire);
+
+// CO2 sensor
+HardwareSerial mySerial(1);
+MHZ19 mhz19;
+
+// Serial.println(__FILE__);
+// Serial.print("DHT12 LIBRARY VERSION: ");
+// Serial.println(DHT12_LIB_VERSION);
+// Serial.println();
 
 #define RX_PIN 16
 #define TX_PIN 17
@@ -9,42 +34,71 @@
 #define GRAPH_MAX 3000
 #define GRAPH_MIN 1
 #define GRAPH_X 0
-#define GRAPH_Y 40
-#define GRAPH_HEIGHT 200
+#define GRAPH_Y 100
+#define GRAPH_HEIGHT 100
 #define GRAPH_WIDTH 320
 #define GRAPH_WARN_HEIGHT 132
-
-MHZ19 myMHZ19;
-HardwareSerial mySerial(1);
+const unsigned long background_color = 0x000000U;
+const unsigned long message_text_color = 0xFFFFFFU;
 TFT_eSprite graph = TFT_eSprite(&M5.Lcd);
 
-void setup(){
+void setup() {
     M5.begin();
-    M5.Lcd.setTextSize(3);
-    Serial.begin(9600);
+    M5.Power.begin();
+    Wire.begin(); // I2Cの初期化
+    dht12.begin();
+    lcd.init();
+    lcd.setFont(&fonts::lgfxJapanGothic_36);
+    lcd.setTextSize(0.5);
+    lcd.setTextColor(message_text_color, background_color);
+    lcd.setBrightness(10);
 
+
+    while (!bmp280.begin(BMP280_I2C_ADDRESS)){  
+      Serial.println("Could not find a valid BMP280 sensor, check wiring!");
+      M5.Lcd.println("Could not find a valid BMP280 sensor, check wiring!");
+      delay(1000);
+    }
+
+    // setup for CO2 sensor
     mySerial.begin(BAUDRATE, SERIAL_8N1, RX_PIN, TX_PIN);
-    myMHZ19.begin(mySerial);
-    myMHZ19.autoCalibration();
+    mhz19.begin(mySerial);
+    mhz19.autoCalibration();
 
     graph.setColorDepth(8);
     graph.createSprite(GRAPH_WIDTH, GRAPH_HEIGHT + 1);
     graph.fillSprite(TFT_BLACK);
-    graph.fillRect(0, 0, GRAPH_WIDTH, GRAPH_WARN_HEIGHT, M5.Lcd.color565(152, 152, 0));
+    graph.fillRect(0, 0, GRAPH_WIDTH, GRAPH_WARN_HEIGHT, M5.Lcd.color565(0, 0, 0));
     graph.pushSprite(GRAPH_X, GRAPH_Y);
 }
 
+
 void loop(){
-    M5.Lcd.setCursor(0, 0);
+    lcd.setCursor(0, 0);
 
-    int CO2 = myMHZ19.getCO2();
-    M5.Lcd.print("CO2: ");
-    M5.Lcd.print(CO2);
-    M5.Lcd.println("ppm ");
+    int CO2 = mhz19.getCO2();
+    float humidity = dht12.readHumidity();
+    float temp = dht12.readTemperature();  
+    float pressure = bmp280.readPressure() / 100.0;
 
+    lcd.printf("CO2: %d ppm\n", CO2);
+    lcd.printf("Temp.: %0.2f °C\n", temp);
+    lcd.printf("humidity: %0.2f %%\n", humidity);
+    lcd.printf("気圧: %0.2f hPa\n", pressure);
+
+    // CO2 graph
     graph.pushSprite(GRAPH_X, GRAPH_Y);
     graph.scroll(-1, 0);
-    graph.fillRect(GRAPH_WIDTH -1, 0, GRAPH_WIDTH, GRAPH_WARN_HEIGHT, M5.Lcd.color565(152, 152, 0));
+    if (CO2 < 800){
+        graph.fillRect(GRAPH_WIDTH -1, 0, GRAPH_WIDTH, GRAPH_WARN_HEIGHT, M5.Lcd.color565(0, 200, 0));
+    }
+    else if (CO2 < 1200){
+        graph.fillRect(GRAPH_WIDTH -1, 0, GRAPH_WIDTH, GRAPH_WARN_HEIGHT, M5.Lcd.color565(152, 152, 0));
+    }
+    else {
+        graph.fillRect(GRAPH_WIDTH -1, 0, GRAPH_WIDTH, GRAPH_WARN_HEIGHT, M5.Lcd.color565(200, 0, 0));
+    }
+    
     int p = CO2 / (GRAPH_MAX / GRAPH_HEIGHT);
     graph.drawPixel(GRAPH_WIDTH -1, GRAPH_HEIGHT - p, TFT_WHITE);
 
